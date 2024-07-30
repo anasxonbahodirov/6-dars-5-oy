5ef48ccdca03dc89ec8993510b6802eb5e935a53
anasxonbahodirov <anasxonbahodirov1@gmail.com>
2024-07-30 11:47:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let a = prompt('Orqa fon ranggini ozgartiring')
+let b = prompt('Text rangini ozgartir')
+let body = document.querySelector('body')
+let text = document.querySelector('p')
+body.style.backgroundColor = a
+body.style.color = b
